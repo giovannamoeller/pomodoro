a6b8fea62ffc1d582720a4069a5716c7c7a48a7e
Giovanna <giovannagmoeller@gmail.com>
2022-01-29 14:55:03 -0300

    creating circular progress view with timer

--- PomodoroApp/ViewController.swift
+++ PomodoroApp/ViewController.swift
@@ -9,21 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
   
-  private var circleLayer = CAShapeLayer()
-  private var progressLayer = CAShapeLayer()
-  private var startPoint = CGFloat(-Double.pi / 2)
-  private var endPoint = CGFloat(3 * Double.pi / 2)
+  var circularProgressView: CircularProgressView!
+  
+  private lazy var timeText: UILabel = {
+    let text = UILabel()
+    text.translatesAutoresizingMaskIntoConstraints = false
+    text.text = "25:00"
+    text.textColor = .white
+    text.font = .systemFont(ofSize: 48.0, weight: .heavy)
+    return text
+  }()
+  
+  private lazy var ovalImageView: UIImageView = {
+    let imageView = UIImageView(image: UIImage(named: "oval"))
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    circularProgressView = CircularProgressView(frame: .zero, duration: 5)
     addSubviews()
     configureUI()
     setUpConstraints()
-    createCircularPath()
-    //setUpCircularProgressBarView()
   }
   
   func addSubviews() {
+    view.addSubview(ovalImageView)
+    view.addSubview(circularProgressView)
+    view.addSubview(timeText)
     
   }
   
@@ -32,43 +46,14 @@ class ViewController: UIViewController {
   }
   
   func setUpConstraints() {
-    
-  }
-  
-  func createCircularPath() {
-    let circularPath = UIBezierPath(arcCenter: CGPoint(x: view.frame.size.width / 2.0, y: view.frame.size.height / 2.0), radius: 80, startAngle: startPoint, endAngle: endPoint, clockwise: true)
-    // circleLayer path defined to circularPath
-    circleLayer.path = circularPath.cgPath
-    // ui edits
-    circleLayer.fillColor = UIColor.clear.cgColor
-    circleLayer.lineCap = .round
-    circleLayer.lineWidth = 10.0
-    circleLayer.strokeEnd = 1.0
-    circleLayer.strokeColor = UIColor.white.cgColor
-    // added circleLayer to layer
-    view.layer.addSublayer(circleLayer)
-    // progressLayer path defined to circularPath
-    progressLayer.path = circularPath.cgPath
-    // ui edits
-    progressLayer.fillColor = UIColor.clear.cgColor
-    progressLayer.lineCap = .round
-    progressLayer.lineWidth = 10.0
-    progressLayer.strokeEnd = 0
-    progressLayer.strokeColor = UIColor.systemIndigo.cgColor
-    
-    view.layer.addSublayer(progressLayer)
-    progressAnimation(duration: 2 * 60)
-  }
-  
-  func progressAnimation(duration: TimeInterval) {
-    // created circularProgressAnimation with keyPath
-    let circularProgressAnimation = CABasicAnimation(keyPath: "strokeEnd")
-    // set the end time
-    circularProgressAnimation.duration = duration
-    circularProgressAnimation.toValue = 1.0
-    circularProgressAnimation.fillMode = .forwards
-    circularProgressAnimation.isRemovedOnCompletion = false
-    progressLayer.add(circularProgressAnimation, forKey: "progressAnim")
+    circularProgressView.center = view.center
+    NSLayoutConstraint.activate([
+      timeText.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      timeText.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      
+      ovalImageView.centerYAnchor.constraint(equalTo: circularProgressView.centerYAnchor),
+      ovalImageView.centerXAnchor.constraint(equalTo: circularProgressView.centerXAnchor),
+    ])
   }
   
 }
