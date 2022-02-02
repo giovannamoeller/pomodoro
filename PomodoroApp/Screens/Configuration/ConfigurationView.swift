54747b6ac7b4efdc96b1f1cf572d67a7de0dc911
Giovanna <giovannagmoeller@gmail.com>
2022-02-01 23:40:15 -0300

    creating configuration view

---
+++ PomodoroApp/Screens/Configuration/ConfigurationView.swift
@@ -0,0 +1,20 @@
+//
+//  ConfigurationView.swift
+//  PomodoroApp
+//
+//  Created by Giovanna Moeller on 01/02/22.
+//
+
+import UIKit
+
+class ConfigurationView: UIView {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+}
---
+++ PomodoroApp/Screens/Configuration/ConfigurationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ConfigurationViewController.swift
+//  PomodoroApp
+//
+//  Created by Giovanna Moeller on 01/02/22.
+//
+
+import UIKit
+
+class ConfigurationViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view = ConfigurationView()
+  }
+}
